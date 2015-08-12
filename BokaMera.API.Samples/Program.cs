bb1e9eade9a485c346f3ac539d462030c8031951
André Johansson <andre.johansson@readsoft.com>
2015-08-12 21:36:10 +0200

    Updated sample with comments

--- BokaMera.API.Samples/Program.cs
+++ BokaMera.API.Samples/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using BokaMera.API.ServiceModel.Dtos;
 using ServiceStack;
 using ServiceStack.MsgPack;
 using ServiceStack.Text;
@@ -8,39 +7,36 @@ namespace BokaMera.API.Samples
 {
    class Program
    {
+      const string ApiUrl = "http://testapi.bokamera.se/";
+
       static void Main(string[] args)
       {
          // Create and configure client
-         // BokaMera API supports MsgPack, prefer that for efficient communication
-         // with the API.
-         // See https://github.com/ServiceStack/ServiceStack/wiki/C%23-client
+         // BokaMera API supports MsgPack, prefer that for efficient communication with the API.
          // See https://github.com/ServiceStack/ServiceStack/wiki/MessagePack-Format
-
-         var apiUrl = "http://testapi.bokamera.se/";
+         // See https://github.com/ServiceStack/ServiceStack/wiki/C%23-client
          //var client = new JsonServiceClient(apiUrl);
-         var client = new MsgPackServiceClient(apiUrl);
-         client.AlwaysSendBasicAuthHeader = true;
+         var client = new MsgPackServiceClient(ApiUrl);
 
          // Authenticate
          client.Headers.Add("x-api-key", "somelongrandomkey");
          client.UserName = "demo@bokamera.se";
          client.Password = "demo12";
 
-         // Call service, this uses Dto from BokaMera's nuget package to return typed responses
+         // Call service, this uses Dto from BokaMera's nuget package to return typed responses.
+         // Check the namespace BokaMera.API.ServiceModel.Dto.* for request and response classes
          Console.WriteLine("Calling service FindBookings...");
-         var response = client.Get(new FindBookings { From = DateTime.UtcNow.AddYears(-1), To = DateTime.UtcNow });
+         var response = client.Get(new BokaMera.API.ServiceModel.Dto.GetApiVersion());
          
+         // Print response
          Console.WriteLine("Response received, printing output...");
          response.PrintDump();
 
          // Logout session
          Console.WriteLine("Logging out...press any key to quit");
          client.Post(new Authenticate { provider = "logout" });
-         
-         // In ServiceStack v4 you can use
-         // See: http://stackoverflow.com/questions/14116882/how-to-logout-authenticated-user-in-servicestack
-         //client.Post(new Authenticate { provider = AuthenticateService.LogoutAction });
 
+         // Wait for keypress
          Console.ReadKey();
       }
    }
