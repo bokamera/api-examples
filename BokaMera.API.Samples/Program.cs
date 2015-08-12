7912d9013c28915c62892154c37af7b004b33e5e
André Johansson <andre.johansson@readsoft.com>
2015-08-12 19:42:16 +0200

    Initial commit, typed sample with autentication and API key
    Internal release only due to unpublished ServiceModel dependency

---
+++ BokaMera.API.Samples/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using BokaMera.API.ServiceModel.Dtos;
+using ServiceStack;
+using ServiceStack.Text;
+
+namespace BokaMera.API.Samples
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         // See https://github.com/ServiceStack/ServiceStack/wiki/C%23-client
+         var client = new JsonServiceClient("http://localhost:59600/");
+         client.Headers.Add("x-api-key", "somelongrandomkey");
+         client.AlwaysSendBasicAuthHeader = true;
+         client.UserName = "demo@bokamera.se";
+         client.Password = "demo12";
+
+         var response = client.Get(new FindBookings { From = DateTime.UtcNow.AddYears(-1), To = DateTime.UtcNow });
+         response.PrintDump();
+         Console.ReadKey();
+      }
+   }
+}
