784f035cdfebc98b6769cb823c4f2241eae207e6
Kristian Andersson <kristian.hbg.andersson@gmail.com>
2017-01-26 11:49:51 +0100

    Updated sample

--- BokaMera.API.Samples/Program.cs
+++ BokaMera.API.Samples/Program.cs
@@ -8,7 +8,9 @@ namespace BokaMera.API.Samples
 {
     class Program
     {
-        const string ApiUrl = "http://localhost:59600/";
+        const string ApiUrlTest = "https://testapi.bokamera.se/";
+        //const string ApiUrlProd = "https://api.bokamera.se/";
+
 
         static void Main(string[] args)
         {
@@ -16,38 +18,32 @@ namespace BokaMera.API.Samples
             // BokaMera API supports MsgPack, prefer that for efficient communication with the API.
             // See https://github.com/ServiceStack/ServiceStack/wiki/MessagePack-Format
             // See https://github.com/ServiceStack/ServiceStack/wiki/C%23-client
-            //var client = new JsonServiceClient(apiUrl);
-            var client = new MsgPackServiceClient(ApiUrl);
-
-            // Authenticate
-            client.Headers.Add("x-api-key", "Somelongrandoapikey"); //You will get this API from the support@bokamera.se
-            //client.UserName = "demo@bokamera.se";
-            //client.Password = "demo12";
-
+            var client = new JsonServiceClient(ApiUrlTest);
+            client.Headers.Add("x-api-key", "YOUR_API_KEY_HERE");
+            
             //Authenticate the user
-            client.Post(new Authenticate() { provider = "login", UserName = "demo@bokamera.se", Password = "demo12" });
+            var response = client.Post<AuthenticateResponse>("authenticate", new
+            {
+                UserName = "username",
+                Password = "password",
+            });
 
+            client.AddHeader("x-ss-id", response.SessionId);
 
             // Call service, this uses Dto from BokaMera's nuget package to return typed responses.
             // Check the namespace BokaMera.API.ServiceModel.Dto.* for request and response classes
             Console.WriteLine("Calling service GetApiVersion...");
-            var response = client.Get(new ApiVersionQuery());
+            var versionResponse = client.Get(new ApiVersionQuery());
 
 
             //Create request object for retrieving bookings
             var request = new BookingQuery
             {
-                BookingStart = new DateTime(2017, 01, 01), //From what date to collect bookings
-                AllCompanyBookings = true,  //If retrieve all bookings for the logged in user (the customer) AND for the company he is admin for (requires the user is a admin, otherwise set false here)
-                IncludeServiceInformation = true, //Will also retrive the Service information on the Booking. If included = false the property will be null (theBooking.Service)
-                IncludeCustomFieldValues = true, //Will also retrive the custom field and values on the Booking (if any exists). If included = false the property will be null (theBooking.CustomFieldValues)
-                StatusIds = null //What statuses to include, if null or empty array only active bookings will be included
+               Id = 370476
             };
-
 
             //Retrieve the bookings. To see what return type see the https://testapi.bokamera.se/swagger-ui/#!/bookings/BookingQuery  
             QueryResponse<BookingQueryResponse> bookings = client.Get<QueryResponse<BookingQueryResponse>>(request);
-
 
             // Print response
             Console.WriteLine("Response received, printing output...");
